fed0bbe99eac7fdad396aae7fa5671946e0cd8f3
DESKTOP-0ORH8IU\chien <chiengaren297@gmail.com>
2021-06-19 00:04:04 +0700

    update dtb

--- hccShop.Data/Extensions/ModelBuilderExtensions.cs
+++ hccShop.Data/Extensions/ModelBuilderExtensions.cs
@@ -10,16 +10,16 @@ namespace hccShop.Data.Extensions
     {
         public static void Seed(this ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<AppConfig>().HasData(
-                new AppConfig() { Key = "HomeTitle", Value = "This is home page of HccShop" },
-                new AppConfig() { Key = "HomeKeyWord", Value = "This is home keyword of HccShop" },
-                new AppConfig() { Key = "HomeDescription", Value = "This is home description of HccShop" }
-                );
-            modelBuilder.Entity<Category>().HasData(
-                new Category() { name = "",IsShowOnHome = true, ParentId = null, SortOrder = 1, Status = Enums.Status.Active, },
-                new Category() { },
-                new Category() { }
-                );
+            //modelBuilder.Entity<AppConfig>().HasData(
+            //    new AppConfig() { Key = "HomeTitle", Value = "This is home page of HccShop" },
+            //    new AppConfig() { Key = "HomeKeyWord", Value = "This is home keyword of HccShop" },
+            //    new AppConfig() { Key = "HomeDescription", Value = "This is home description of HccShop" }
+            //    );
+            //modelBuilder.Entity<Category>().HasData(
+            //    new Category() { name = "", IsShowOnHome = true, ParentId = null, SortOrder = 1, Status = Enums.Status.Active, },
+            //    new Category() { },
+            //    new Category() { }
+            //    );
 
         }
     }
